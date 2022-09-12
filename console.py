de464365edba32d7168958119f3cc1821a41df03
55Nights <1905587@students.kca.ac.ke>
2022-09-12 19:24:49 +0300

    Be pycodestyle compliant!

--- console.py
+++ console.py
@@ -183,7 +183,7 @@ class HBNBCommand(cmd.Cmd):
             argument = argument + arg + " "
         try:
             eval('self.do_' + cmd + '(argument)')
-        except Exception:
+        except (Exception):
             print("** invalid command **")
 
 
